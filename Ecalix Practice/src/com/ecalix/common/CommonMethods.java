44c90c16f7dc7ca601cf57ea30b2568030ddc2a8
Donghui <rambo_lu@hotmail.com>
2015-06-03 15:32:36 -0700

    Ecalix practice

---
+++ Ecalix Practice/src/com/ecalix/common/CommonMethods.java
@@ -0,0 +1,46 @@
+package com.ecalix.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+public class CommonMethods {
+	public String sURL ="http://107.170.213.234/catalog/";
+	public WebDriver driver;
+	
+	public void OpenBrowser(){
+		driver= new FirefoxDriver();
+		System.out.println("Open a Firefox Browser");
+	}
+	
+	public void GotoUrl(){
+		driver.get(sURL);
+		System.out.println("navigating to"+sURL);
+
+	}
+	public void CloseBrowser(){
+		driver.quit();
+		System.out.println("Browser is close");
+		
+	}
+	public void clickYourselflink(){
+		driver.findElement(By.xpath("//*[@id='bodyContent']/div/div[1]/a[1]/u")).click();
+		System.out.println("click you self link is clicked");
+	}
+	public void clicklogoff(){
+		driver.findElement(By.xpath("//*[@id='tdb4']/span")).click();
+		System.out.println("log off button is clicked");
+	}
+	public void Login(String UserName, String Password){
+		driver.findElement(By.xpath("//*[@id='bodyContent']/div[2]/div/form/table/tbody/tr[1]/td[2]/input")).sendKeys(UserName);
+		System.out.println("User Name"+UserName+"entered");
+		driver.findElement(By.xpath("//*[@id='bodyContent']/div[2]/div/form/table/tbody/tr[2]/td[2]/input")).sendKeys(Password);
+		System.out.println("Password"+Password+"entered");
+		driver.findElement(By.xpath("//*[@id='tdb5']/span[2]")).click();
+		System.out.println("Submit button is clicked");
+	}
+	
+
+}
---
+++ Ecalix Practice/src/com/ecalix/tests/LoginTest.java
@@ -0,0 +1,50 @@
+package com.ecalix.tests;
+
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.AssertJUnit;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.ecalix.common.CommonMethods;
+import com.opera.core.systems.OperaSettings.Capability;
+
+
+public class LoginTest extends CommonMethods {
+	public String nodeURL ="http://10.0.0.6:4444/wd/hub";
+	@BeforeMethod
+	public void setUp() throws MalformedURLException {
+		DesiredCapabilities capability= DesiredCapabilities.chrome();
+		capability.setBrowserName("chrome");
+		capability.setPlatform(Platform.VISTA);
+		driver = new RemoteWebDriver(new URL(nodeURL),capability);
+//		OpenBrowser();
+		GotoUrl();
+	}
+	@AfterMethod
+	public void teardown(){
+		CloseBrowser();
+	}
+	
+	@Test
+	public void Login01(){
+		clickYourselflink();
+		Login("ecalix@test.com","test123");
+		String ExpectedText="Welcome to iBusiness";
+		System.out.println("ExpectedText is"+ExpectedText);
+		String ActualText=driver.findElement(By.xpath("//*[@id='bodyContent']/h1")).getText();
+	    System.out.println("AcutalText is"+ActualText);
+	    AssertJUnit.assertEquals(ExpectedText, ActualText);
+	    clicklogoff();
+	}
+
+}
